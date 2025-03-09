fe0532a753d79dfa09a67eb1d5ef8dd449c7fecd
Amalia Haible <florian@haible.de>
2025-03-09 03:13:12 +0100

    terminal stuff, updates

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,8 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("t", "<C-h>", "<C-\\><C-n>", { silent = true })
-vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>", {silent=true}) 
-vim.keymap.set('n', '<A-o>', '<CMD>lua require("FTerm").open()<CR>')
-vim.keymap.set('t', '<A-c>', '<C-\\><C-n><CMD>lua require("FTerm").close()<CR>')
+vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>", { silent = true })
 vim.keymap.set('t', '<A-e>', '<C-\\><C-n><CMD>lua require("FTerm").exit()<CR>')
 vim.keymap.set('n', '<A-p>', '<CMD>lua require("FTerm").toggle()<CR>')
 vim.keymap.set('t', '<A-p>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
+vim.keymap.set({ 't', 'n' }, '<A-n>', function()
+    if vim.fn.has("Linux") then
+        require('snacks.terminal').toggle('btop', { win = { width = 0.9, height = 0.9 } })
+    end
+end, { desc = 'Open btop in a floating terminal' })
+vim.keymap.set({ 't', 'n' }, '<A-m>', function()
+    if vim.fn.has("Linux") then
+        require('snacks.terminal').toggle('btop ', { win = { width = 0.5, height = 0.5 } })
+    end
+end, { desc = 'Open small btop in a floating terminal' })
+
+
--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,8 +1,12 @@
 return {
-	"numToStr/FTerm.nvim",
-	config = function()
-		require("FTerm").setup({
-			cmd = "powershell",
-		})
-	end,
+    "numToStr/FTerm.nvim",
+    config = function()
+        local shell = "powershell"
+        if vim.fn.has("Linux") then
+            shell = "zsh"
+        end
+        require("FTerm").setup({
+            cmd = shell,
+        })
+    end,
 }
