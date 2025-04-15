e2804c891ae757ffaae5599abd78f4947cfc9aba
Amalia Haible <florian@haible.de>
2025-04-15 17:16:34 +0200

    theme change

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -16,7 +16,35 @@ return {
         "EdenEast/nightfox.nvim",
         name = "nightfox",
         config = function()
-            vim.cmd("colorscheme nightfox")
+            require("nightfox").setup({
+                palettes = {
+                    -- Custom duskfox with black background
+                    nordfox = {
+                        -- bg0 = "#282c34",
+                        bg1 = "#282c34",
+                        -- bg0 = "#FF0000",
+                        -- bg2 = "#00FF00",
+                        -- bg3 = "#0000FF",
+                        -- bg4 = "#010101",
+
+                        -- bg2 = "#282c34",
+                        -- bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
+                        -- bg3 = "#121820", -- 55% darkened from stock
+                        -- sel0 = "#131b24", -- 55% darkened from stock
+                    },
+                },
+                -- specs = {
+                --   duskfox = {
+                --     inactive = "#090909", -- Slightly lighter then black background
+                --   },
+                -- },
+                -- groups = {
+                --   all = {
+                --     NormalNC = { fg = "fg1", bg = "inactive" }, -- Non-current windows
+                --   },
+                -- },
+            })
+            vim.cmd("colorscheme nordfox")
         end
     },
     { "folke/tokyonight.nvim" },
---
+++ test.lua
@@ -0,0 +1,2 @@
+    local palette = require('nightfox.palette').load("nordfox")
+    print(vim.inspect(palette))
